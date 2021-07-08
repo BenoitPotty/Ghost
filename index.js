6da1668ab50796b414f597f1e09989b611940432
Benoit Potty <benoit.potty@liip.ch>
2021-07-08 15:24:06 +0200

    Startup log refactoring

--- index.js
+++ index.js
@@ -1,9 +1,2 @@
-/* eslint-disable no-console */
-console.log('');
-console.log('\x1b[32m','####################################################');
-console.log('\x1b[32m','#                                                  #');
-console.log('\x1b[32m','#        RUNNING GHOST INSTANCE BY LIIP            #');
-console.log('\x1b[32m','#                                                  #');
-console.log('\x1b[32m','####################################################');
-console.log('\x1b[0m');
+require('./liip_stratup');
 require('./ghost');
