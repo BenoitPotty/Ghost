cb48ddd18547fc0ecea1e7f03caa1fef1ef0c3d0
Benoit Potty <benoit.potty@liip.ch>
2021-07-08 09:57:39 +0200

    Display custom Liip message @ startup

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+/* eslint-disable no-console */
+console.log('');
+console.log('\x1b[32m','####################################################');
+console.log('\x1b[32m','#                                                  #');
+console.log('\x1b[32m','#        RUNNING GHOST INSTANCE BY LIIP            #');
+console.log('\x1b[32m','#                                                  #');
+console.log('\x1b[32m','####################################################');
+console.log('\x1b[0m');
 require('./ghost');
