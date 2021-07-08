c7d55dd730b056490bbbb8af45cadaa98c7c201a
Benoit Potty <benoit.potty@liip.ch>
2021-07-08 09:32:30 +0200

    Iterate through installed themes and look for helpers defined there

--- core/frontend/helpers/index.js
+++ core/frontend/helpers/index.js
@@ -1,6 +1,6 @@
 const glob = require('glob');
 const path = require('path');
-
+const config = require('../../shared/config');
 const helpers = {};
 
 // We use glob here because it's already a dependency
@@ -10,6 +10,13 @@ let helperFiles = glob.sync('!(index).js', {cwd: __dirname});
 helperFiles.forEach((helper) => {
     let name = helper.replace(/.js$/, '');
     helpers[name] = require(path.join(__dirname, helper));
+});
+
+// Go through the theme directory
+let customThemesHelperFiles = glob.sync(path.join(config.getContentPath('themes'), '**/helpers/!(index).js'));
+customThemesHelperFiles.forEach((helper) => {
+    let name = helper.match(/[ \w-]+?(?=\.)/)[0];
+    helpers[name] = require(helper.replace('.js', ''));
 });
 
 module.exports = helpers;
