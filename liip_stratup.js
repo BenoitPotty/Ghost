98baec246205ac1dfec622fcdf2a08a5fb149dcb
Benoit Potty <benoit.potty@liip.ch>
2021-07-16 10:57:09 +0200

    Include Ghost version in Dockerfile

--- liip_stratup.js
+++ liip_stratup.js
@@ -11,5 +11,5 @@ console.log(`NODE_ENV : ${process.env.NODE_ENV}`);
 console.log(`GHOST_CLI_VERSION : ${process.env.GHOST_CLI_VERSION}`);
 console.log(`GHOST_INSTALL : ${process.env.GHOST_INSTALL}`);
 console.log(`GHOST_CONTENT : ${process.env.GHOST_CONTENT}`);
-console.log(`HOST_VERSION : ${process.env.HOST_VERSION}`);
+console.log(`GHOST_VERSION : ${process.env.GHOST_VERSION}`);
 console.log('\x1b[0m');
