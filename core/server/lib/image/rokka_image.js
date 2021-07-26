042ef619b6438bcea2195ba8a5a4bf3d742e5dbb
Benoit Potty <benoit.potty@liip.ch>
2021-07-26 15:15:13 +0200

    Refactoring

--- core/server/lib/image/rokka_image.js
+++ core/server/lib/image/rokka_image.js
@@ -2,7 +2,7 @@ const rokka = require('../../../frontend/helpers/rokka_image');
 const logging = require('@tryghost/logging');
 const cheerio = require('cheerio');
 
-module.exports = function rokka_image(originalRenderer) {
+module.exports = function rokkaImage(originalRenderer) {
     return function ({payload, env: {dom}, options}) {
         const figure = originalRenderer({payload, env: {dom}, options});
 
@@ -11,12 +11,12 @@ module.exports = function rokka_image(originalRenderer) {
 
             //Use the rokka_image helper function to uniformize image generation
             let imgString = rokka(payload.src, {hash: {altText: payload.alt || ''}});
-            const rokkaImage = cheerio.load(imgString.toHTML());
+            const rokkaImg = cheerio.load(imgString.toHTML());
 
-            currentImage.setAttribute('src', rokkaImage('img').attr('src'));
-            currentImage.setAttribute('srcset', rokkaImage('img').attr('srcset'));
-            currentImage.setAttribute('alt', rokkaImage('img').attr('alt'));
-            currentImage.setAttribute('sizes', rokkaImage('img').attr('sizes'));
+            currentImage.setAttribute('src', rokkaImg('img').attr('src'));
+            currentImage.setAttribute('srcset', rokkaImg('img').attr('srcset'));
+            currentImage.setAttribute('alt', rokkaImg('img').attr('alt'));
+            currentImage.setAttribute('sizes', rokkaImg('img').attr('sizes'));
         } catch (error) {
             logging.error(`Impossible to render Rokka Image. Reason : ${error}`);
         }
