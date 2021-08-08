ee29f13223dba2cdc7140a58c18d84a9b44e3c3f
Benoit Potty <benoit.potty@liip.ch>
2021-08-08 14:58:30 +0200

    ELLEXX : Special gallery using only Tailwind

--- core/server/lib/image/rokka-gallery.js
+++ core/server/lib/image/rokka-gallery.js
@@ -1,43 +1,93 @@
-const rokkaUtils = require('./rokka-utils');
-const twFirstELement = 'absolute inset-0 w-screen h-screen text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-0 slide';
-const twOtherElements = 'absolute inset-0 w-screen h-screen text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-full slide';
+const controlCommonClasses = 'w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden font-bold text-black hover:text-white rounded-full bg-white hover:bg-blue-700 leading-tight text-center z-10 inset-y-0 my-auto';
 
 module.exports = function rokkaGallery() {
     return function ({payload, env: {dom}}) {
         const gallery = dom.createElement('div');
         gallery.setAttribute('id', 'ellexx_gallery');
-        gallery.setAttribute('class', 'relative');
+        gallery.setAttribute('class', 'carousel relative rounded relative overflow-hidden shadow-xl');
 
+        const slides = dom.createElement('div');
+        slides.setAttribute('id', 'ellexx_gallery_slides');
+        slides.setAttribute('class', 'carousel-inner relative overflow-hidden w-full');
+        gallery.appendChild(slides);
+
+        const indicators = dom.createElement('ol');
+        indicators.setAttribute('class', 'carousel-indicators');
+
+        const imgCount = payload.images.length;
         payload.images.forEach((image, index) => {
-            const img = dom.createElement('img');
-            rokkaUtils.copyAttributes(image.src, payload.caption ? `${payload.caption} ${index}` : `Gallery image ${index}`, img);
+            const slideNumber = index + 1;
 
-            const galleryItem = dom.createElement('div');
-            galleryItem.setAttribute('id', `ellexx_gallery_item_${index}`);
-            galleryItem.setAttribute('class', `ellexx-gallery-item ${image.width < image.height ? 'is-portrait' : 'is-landscape'} ${index === 0 ? twFirstELement : twOtherElements}`);
-            galleryItem.appendChild(img);
+            slides.appendChild(createInput(dom, slideNumber));
 
-            gallery.appendChild(galleryItem);
+            slides.appendChild(createSlide(dom, image));
+
+            slides.appendChild(createPreviousControl(dom, slideNumber, imgCount));
+
+            slides.appendChild(createNextControl(dom, slideNumber, imgCount));
+
+            createIndicator(dom, slideNumber, indicators);
         });
 
-        const prevButton = dom.createElement('div');
-        prevButton.setAttribute('class', 'fixed bottom-0 right-0 bg-white w-16 h-16 flex items-center justify-center text-black');
-        prevButton.setAttribute('id', 'ellexx_gallery_btn_prev');
-        gallery.appendChild(prevButton);
-
-        const nextButton = dom.createElement('div');
-        nextButton.setAttribute('class', 'fixed bottom-0 right-0 bg-white w-16 h-16 flex items-center justify-center text-black');
-        nextButton.setAttribute('id', 'ellexx_gallery_btn_next');
-        gallery.appendChild(nextButton);
+        slides.appendChild(indicators);
 
         return gallery;
     };
 };
 
-/* <div class="relative">
-    <div class="absolute inset-0 w-screen h-screen text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-0 slide">Hello</div>
-    <div class="absolute inset-0 w-screen h-screen text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-full slide">There</div>
-    <div class="absolute inset-0 w-screen h-screen text-white flex items-center justify-center text-5xl transition-all ease-in-out duration-1000 transform translate-x-full slide">Booya!</div>
-    <div class="fixed bottom-0 right-0 bg-white w-16 h-16 flex items-center justify-center text-black">&#x276F;</div>
-    <div class="fixed bottom-0 right-0 bg-white w-16 h-16 mr-16 border-r border-gray-400 flex items-center justify-center text-black">&#x276E;</div>
-</div> */
+function createInput(dom, slideNumber) {
+    const input = dom.createElement('input');
+    input.setAttribute('id', `carousel-${slideNumber}`);
+    input.setAttribute('class', `carousel-open`);
+    input.setAttribute('type', 'radio');
+    input.setAttribute('name', 'carousel');
+    input.setAttribute('aria-hidden', 'true');
+    input.setAttribute('hidden', '');
+    if (slideNumber === 1) {
+        input.setAttribute('checked', 'checked');
+    }
+    return input;
+}
+
+function createSlide(dom, image) {
+    const slide = dom.createElement('div');
+    slide.setAttribute('class', `carousel-item absolute opacity-0 bg-center`);
+    slide.setAttribute('style', `height:500px; background-image: url(${image.src})`);
+    return slide;
+}
+
+function createPreviousControl(dom, slideNumber, slideCount) {
+    const previous = dom.createElement('label');
+    previous.setAttribute('for', `carousel-${slideNumber === 1 ? slideCount : slideNumber - 1}`);
+    previous.setAttribute('class', `control-${slideNumber} ${controlCommonClasses} left-0`);
+    previous.appendChild(createControlContent(dom, '<'));
+    return previous;
+}
+
+function createNextControl(dom, slideNumber, slideCount) {
+    const next = dom.createElement('label');
+    next.setAttribute('for', `carousel-${slideNumber === slideCount ? 1 : slideNumber + 1}`);
+    next.setAttribute('class', `next control-${slideNumber} ${controlCommonClasses} right-0`);
+    next.appendChild(createControlContent(dom, '>'));
+    return next;
+}
+
+function createControlContent(dom, contentText) {
+    const content = dom.createElement('i');
+    content.setAttribute('class', 'mt-3');
+    content.appendChild(dom.createTextNode(contentText));
+    return content;
+}
+
+function createIndicator(dom, slideNumber, indicators) {
+    const indicator = dom.createElement('li');
+    indicator.setAttribute('class', 'inline-block mr-3');
+
+    const indicatorLabel = dom.createElement('label');
+    indicatorLabel.setAttribute('for', `carousel-${slideNumber}`);
+    indicatorLabel.setAttribute('class', `carousel-bullet cursor-pointer block text-4xl text-white hover:text-blue-700`);
+    const indicatorText = dom.createTextNode('•');
+    indicatorLabel.appendChild(indicatorText);
+    indicator.appendChild(indicatorLabel);
+    indicators.appendChild(indicator);
+}
