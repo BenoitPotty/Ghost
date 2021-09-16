c31f1c90cf05a90841d369ea51d121bad512e02b
Benoit Potty <benoit.potty@liip.ch>
2021-09-16 17:01:13 +0200

    ELLEXX-185 : Support new fields

--- core/server/lib/ellexx/mtf.js
+++ core/server/lib/ellexx/mtf.js
@@ -5,8 +5,12 @@ const en = {
     background: 'Background',
     generosity_min: 'Sringy',
     generosity_max: 'Generous',
+    type_min: 'Saver',
+    type_max: 'Investor',
     assets_min: 'Saving account',
     assets_max: 'Action',
+    payment_method_min: 'Cash',
+    payment_method_max: 'Digital Payment',
     decision_min: 'Budget book',
     decision_max: 'Gut feeling',
     currency_min: 'Francs',
@@ -15,7 +19,10 @@ const en = {
     children: 'Children',
     location: 'Location',
     largest_expense_item: 'Largest expense item',
-    income: 'Income'
+    income: 'Income',
+    job: 'Job',
+    debts: 'Debts',
+    capital: 'Capital'
 };
 
 const de = {
@@ -23,8 +30,12 @@ const de = {
     background: 'Hintergrund',
     generosity_min: 'Knauserig',
     generosity_max: 'Grosszügig',
+    type_min: 'Sparer:in',
+    type_max: 'Investor:in',
     assets_min: 'Sparkonto',
     assets_max: 'Aktion',
+    payment_method_min: 'Cash',
+    payment_method_max: 'Digital Payment',
     decision_min: 'Haushaltsbuch',
     decision_max: 'Bauchgefühl',
     currency_min: 'Franken',
@@ -33,7 +44,10 @@ const de = {
     children: 'Kinder',
     location: 'Ort',
     largest_expense_item: 'Ausgabeposten',
-    income: 'Einkommen'
+    income: 'Einkommen',
+    job: 'Beruf',
+    debts: 'Schulden',
+    capital: 'Vermögen'
 };
 class MtfRenderer extends EllexxCardBaseRenderer {
     constructor() {
@@ -46,16 +60,21 @@ class MtfRenderer extends EllexxCardBaseRenderer {
         const personality = this.appendBlock('personality');
         this.appendBlock('personality_title', personality, this.getMultilangContent('personality'));
         this.appendPersonality('generosity', personality);
-        this.appendPersonality('assets', personality);
+        this.appendPersonality('type', personality);
         this.appendPersonality('decision', personality);
+        this.appendPersonality('payment_method', personality);
+        this.appendPersonality('assets', personality);
         this.appendPersonality('currency', personality);
         const background = this.appendBlock('background');
         this.appendBlock('backgroud_title', background, this.getMultilangContent('background'));
         this.appendBackground('age', background);
         this.appendBackground('children', background);
         this.appendBackground('location', background);
-        this.appendBackground('largest_expense_item', background);
+        this.appendBackground('job', background);
         this.appendBackground('income', background);
+        this.appendBackground('debts', background);
+        this.appendBackground('largest_expense_item', background);
+        this.appendBackground('capital', background);
         return this.root;
     }
 
@@ -72,7 +91,7 @@ class MtfRenderer extends EllexxCardBaseRenderer {
         const itemValue = this.getData(item);
         if (itemValue) {
             const criteriaContainer = this.appendBlock(item, parent, '');
-            this.appendSpan(`${item}_title`, criteriaContainer, this.getMultilangContent(item), 'mtf-background_label');
+            this.appendSpan(`${item}_title`, criteriaContainer, `${this.getMultilangContent(item)}:`, 'mtf-background_label');
             this.appendSpan(`${item}_value`, criteriaContainer, itemValue, 'mtf-background_value');
         }
     }
